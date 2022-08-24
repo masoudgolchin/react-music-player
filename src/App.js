6f586d78b45420fa3cfdd9291948497a8ab68cc0
Masoud <masoud@markitdone.com>
2022-08-24 11:03:54 +0430

    add: application data in data.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import data from "./data";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(() => {
+    return songs.filter((song) => song.active === true);
+  });
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong[0]} />
       <Player />
     </div>
   );
